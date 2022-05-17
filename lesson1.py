3ef14c27452eac6836e43494890cf23bbcf131b5
JackJr222 <wesleyjackjr@gmail.com>
2022-05-17 11:41:41 +0300

    assignment1.py

---
+++ lesson1.py
@@ -0,0 +1,15 @@
+print ('Hello World')
+#Python basics
+#Python has no semi colon
+#This is a comment
+
+#This is a 
+#multiline
+#comment
+
+print ("My name is JackJr")
+print ("I'm from South b")
+print ("I'm currently attending a boot camp on STEM inspiration")
+print ("This is the 2nd day")
+print("Python is the language being undertaken at the camp")
+print("So far so good")
