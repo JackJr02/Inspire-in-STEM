f5d13566734b929192a39d7c514d3025b1200ac2
JackJr222 <wesleyjackjr@gmail.com>
2022-05-18 09:09:05 +0300

    assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -4,14 +4,14 @@ pi=3.142
 radius=7
 area=pi*radius*radius
 
-print ("The area of the circle is " + str(area ))
+print ("The area of the circle is " + str(area ) + "cm.sq")
 
 
 #Volume of a cube
 length=4
 volume=length*length*length
 
-print("The volume of the cube is" + str(volume))
+print("The volume of the cube is" + str(volume) + "cm.sq")
  
 
 
@@ -21,7 +21,7 @@ height=4
 PI=3.142
 Vol= PI*radius*radius*height
 
-print ("The volume of the cylinder is " + str(Vol))
+print ("The volume of the cylinder is " + str(Vol) + "cm.sq")
 
 #Surface Area of a Cylinder
 
@@ -31,7 +31,7 @@ diameter=14
 height=3.5
 surfaceArea=2*pI*radius*radius + pI*diameter*height
 
-print("The surface area of the cylinder is" + str(surfaceArea))
+print("The surface area of the cylinder is " + str(surfaceArea) + "cm.sq")
 
 
 
