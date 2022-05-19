4b5a3f4f335e90aa528e6e44ac7ca5a63d9e0bab
JackJr222 <wesleyjackjr@gmail.com>
2022-05-19 11:16:58 +0300

    lesson7.py

---
+++ lesson7.py
@@ -0,0 +1,38 @@
+#Lists
+
+from calendar import monthrange
+from pydoc import plain
+from socket import MSG_DONTROUTE
+
+motorcycle_owner= "MojoJojo"
+plate_number=["_d123", "_y123", "_b123", "_h123"]
+motorcycles=["ducati","yamaha","boxer","honda"]
+print(motorcycles [0] + str ( plate_number[0]),motorcycles [1] + str( plate_number[1]), motorcycles[2] + str( plate_number[2]), motorcycles[3] + str( plate_number[3] ) )
+print (f"My name is { motorcycle_owner} and I own motorcycle plate number {plate_number[0]}")
+
+
+#Accessing List Items using the Index
+
+print (motorcycles[0])
+
+#Changing Elements In a List
+
+motorcycles[0]="bugatti"
+print (motorcycles)
+print ("I love" + str ( motorcycles[0]))
+
+#Adding Elements in a List(.append)
+
+motorcycles.append ("ktm")
+print(motorcycles)
+
+#Deleting an Item from a List(del)
+del motorcycles[0]
+print (motorcycles)
+
+#Pop() method is used to delete the first item
+popped_motorcycles = motorcycles.pop()
+
+#Remove() method
+motorcycles.remove ("yamaha")
+print (motorcycles)
---
+++ lesson8.py
@@ -0,0 +1,17 @@
+#Loops
+school = "Joy", "Hope", "Mercy"
+pupil = "_Mark", "_Samuel", "_James"
+
+#Longest method
+print(f"I am {pupil[0]} and i school at {school[0]}")
+print(f"I am {pupil[1]} and i school at {school[1]}")
+print(f"I am {pupil[2]} and i school at {school[2]}")
+
+#Simplified method(For Loop)
+for pupil in pupil:
+    print(f"Hello, My name is {pupil}")
+
+for school in school:
+    print(f"I school at {school}")
+
+    print(f"Hello, my name is {pupil} and I school at {school}")
