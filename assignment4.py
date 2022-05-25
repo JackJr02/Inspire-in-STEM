96b416b24096ca33c5884e7f398280a420e0fd96
JackJr222 <wesleyjackjr@gmail.com>
2022-05-25 09:40:59 +0300

    assignment4.py

---
+++ assignment4.py
@@ -0,0 +1,15 @@
+#Create a list of vehicles with the following
+#BMW, Toyota, Audi, Mercedes, Jeep
+#Using if statements, find a jeep in the list and convert it into upper case
+
+vehicles= ["bmw", "toyota", "mercedes", "jeep"]
+
+#Using a for loop to print all vehicles
+
+for vehicle in vehicles:
+    if (vehicle=="jeep"):
+      print(vehicle.upper())
+    
+        
+
+
